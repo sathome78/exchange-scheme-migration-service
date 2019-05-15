b99898ccd25149bbc53122ca24dbb2018e3624a2
olegkostiukevych <oleg.kostiukevych@upholding.biz>
2019-05-15 14:19:44 +0300

    DEVEX-2205 (create empty project)

---
+++ src/main/java/me/exrates/schememigrationservice/SchemeMigrationServiceApplication.java
@@ -0,0 +1,13 @@
+package me.exrates.schememigrationservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchemeMigrationServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SchemeMigrationServiceApplication.class, args);
+    }
+
+}
