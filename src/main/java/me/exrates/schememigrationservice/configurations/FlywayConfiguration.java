1cfed7312efa6aa0152a7f8a5c1ef5412d36b3a0
olegpodolian <oleg.podolian@upholding.biz>
2019-06-12 17:58:13 +0300

    clean import

--- src/main/java/me/exrates/schememigrationservice/configurations/FlywayConfiguration.java
+++ src/main/java/me/exrates/schememigrationservice/configurations/FlywayConfiguration.java
@@ -16,7 +16,6 @@ import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Profile;
 import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
 import org.springframework.core.env.Environment;
 
